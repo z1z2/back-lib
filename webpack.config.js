9989d364443cab710f2bc2abd34ac197d4587823
zhang1zhe2 <zhang1zhe2@139.com>
2019-01-26 20:50:01 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+'use strict';
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    globalObject: 'typeof self !== \'undefined\' ? self : this',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'build/Release'),
+    filename: 'back-lib.js',
+  },
+};
